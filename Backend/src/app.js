d49f3797cb7e4935769d445e129c3c3ab4dfab19
Harshit Anand <harshit562005@gmail.com>
2026-06-27 19:49:25 +0530

    Update CORS for Vercel deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth.routes');
-const foodRoutes = require('./routes/food.routers');
-const foodPartnerRoutes = require('./routes/food-partner.routes');
-const cors = require('cors');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes");
+const foodRoutes = require("./routes/food.routers");
+const foodPartnerRoutes = require("./routes/food-partner.routes");
+const cors = require("cors");
 
 const app = express();
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://your-frontend.vercel.app'  
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://reel-bite-leh28krk0-harshitanand5285.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => { 
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/food', foodRoutes);
-app.use('/api/food-partner', foodPartnerRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/food", foodRoutes);
+app.use("/api/food-partner", foodPartnerRoutes);
 
-module.exports = app; 
+module.exports = app;
